5d4ea0010284db3ca63a7a05f7ca2d7d4f667b1d
Lisa Stillwell <lisa@renci.org>
2019-06-21 15:54:01 -0400

    Use different years census block groups for new ACS data
    Use different years census block groups for new ACS data

--- data/load-census_block_grp-shp.sh
+++ data/load-census_block_grp-shp.sh
@@ -1,1 +1,2 @@
-shp2pgsql -s 4269 /projects/datatrans/ACS_Data/USBlockGroups2017/USBlockGroups2017.shp public.census_block_grps | psql -d socio_econ -U datatrans
+shp2pgsql -s 4269 /projects/datatrans/ACS_Data/USBlockGroups2011/USBlockGroups2011.shp public.census_block_grps_2011 | psql -d socio_econ -U datatrans
+shp2pgsql -s 4269 /projects/datatrans/ACS_Data/USBlockGroups2016/USBlockGroups2016.shp public.census_block_grps_2016 | psql -d socio_econ -U datatrans
